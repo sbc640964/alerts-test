22d4685e499802562f5112acf070a1df602f2e43
abc640964 <sbc640964@gmail.com>
2025-09-21 10:22:42 +0300

    fix alert service to handle JSON response and clean up BOM characters

--- app/AlertService.php
+++ app/AlertService.php
@@ -15,11 +15,15 @@ class AlertService
                 ->withHeaders([
                     'Referer' => 'https://www.oref.org.il',
                     'X-Requested-With' => 'XMLHttpRequest',
+                    'Content-Type' => 'application/json',
                 ])
                 ->withUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36')
                 ->get('https://www.oref.org.il/warningMessages/alert/Alerts.json');
             if($res->successful()) {
-                $json = $res->json();
+                $body = $res->body();
+                $clean = preg_replace('/^\xEF\xBB\xBF|\x00|\x1A/', "", $body);
+                $json = json_decode($clean, true);
+
                 if($json){
                     Log::error('New Alert', $json);
                     if(in_array($json['cat'], ['10', '1', '14'])) {
